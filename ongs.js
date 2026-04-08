e58224b2cb73c2774e99c6c5f5d316a4018d1755
brunoluiz <bruno_d_luiz@estudante.sesisenai.org.br>
2026-04-08 09:05:53 -0300

    add - informações iniciais

--- ongs.js
+++ ongs.js
@@ -2,13 +2,30 @@ document.getElementById("formAdocao").addEventListener("submit", function(e) {
 e.preventDefault();
 
 let nome = document.getElementById("nome").value;
+
 let email = document.getElementById("email").value;
+
 let moradia = document.getElementById("moradia").value;
+
 let quintal = document.querySelector('input[name="quintal"]:checked');
+
+let pet = document.querySelector('input[name="pet"]:checked');
 
 if(nome.length < 3) return alert("Nome Inválido");
 
 document.getElementById("resultado").innerHTML = "cadastro realizado com sucesso<br> " +"Nome:" +
 nome;
+let horas = document.querySelector('input[name="horas"]:checked');
+
+let cpf = document.getElementById("cpf").value;
+
+
+let telefone = document.getElementById("telefone").value;
+if(telefone.length < 8) return alert("Numero Inválido");
+
+
+
+
+
 
 });
